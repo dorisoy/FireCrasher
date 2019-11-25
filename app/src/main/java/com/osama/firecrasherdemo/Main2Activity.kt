54e61bc8abfa3565e36c9ed445f73204e8ea2a05
Kristiyan Petrov <petrov.kristiyan@gmail.com>
2019-11-25 14:32:44 +0100

    update to androidx

--- app/src/main/java/com/osama/firecrasherdemo/Main2Activity.kt
+++ app/src/main/java/com/osama/firecrasherdemo/Main2Activity.kt
@@ -2,7 +2,7 @@ package com.osama.firecrasherdemo
 
 import android.os.Bundle
 import android.os.Handler
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main2.*
 
 class Main2Activity : AppCompatActivity() {
